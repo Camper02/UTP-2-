7e14052bac5e2641e326d98f83c9473be1b56fc3
Администратор <@MININT-BUM8O7A>
2023-06-21 00:47:27 +0200

    Добавьте файлы проекта.

---
+++ UTP-2 (1)/UTP-2 (1).cpp
@@ -0,0 +1,15 @@
+﻿#include "UTP-2 (1).h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string  inp = "Hello, my name is Ivan. ";
+    Encryption encrypt_class = 28513;
+    /*cout << encrypt_class.show_key()<<endl;*/
+    inp = encrypt_class.encode(inp);
+    cout << inp << endl;
+    inp = encrypt_class.decode(inp);
+    cout << inp << endl;
+    return 0;
+}
