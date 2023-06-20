b18aa5c4a4fee473ed5a182d06aee94a4785c5b6
Шуманович Егор <78348960+Camper02@users.noreply.github.com>
2023-06-21 01:06:42 +0200

    Add files via upload

---
+++ UTP-2 (2)/UTP-2 (2).cpp
@@ -0,0 +1,26 @@
+﻿#include "UTP-2 (2).h"
+
+using namespace std;
+
+int main()
+{
+    const int n = 4;
+    short int A[n] = { 2, -7, 3, 5 };
+    short int B[n];
+
+    cout << "A: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    Copy(A, B, n);
+
+    cout << "B: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << B[i] << " ";
+    }
+    return 0;
+}
---
+++ UTP-2 (2)/UTP-2 (2).h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <iostream>
+
+
+using namespace std;
+
+template <typename arr>
+void Copy(arr* input, arr* output, int l)
+{
+    const long long* a = (const long long*)input;
+    long long* b = (long long*)output;
+    for (int i = 0; i < (sizeof(input[l]) * l / sizeof(long long)); i++)
+    {
+        *(b++) = *(a++);
+    }
+    char* a2 = (char*)a;
+    char* b2 = (char*)b;
+    for (int i = 0; i < (sizeof(input[l]) * l) % sizeof(long long); i++)
+    {
+        *(b2++) = *(a2++);
+    }
+}
